7b23512ae317dcbfc9da297bd50c18d42397bddd
@d4lí ~ coding <d4li@brunovinicius.local>
2023-07-10 23:29:31 -0300

    rm MainProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
-import ThemeDefault from '@/themes/default';
-import { ThemeProvider } from './themes/default/context/ThemeContext';
-import { MainProvider } from '@/providers/Main';
-import { ProductProvider } from './providers/Product';
-import { OrderProvider } from './providers/Order';
-import '@/App.scss';
-import 'react-toastify/dist/ReactToastify.css';
-
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import ThemeDefault from '@/themes/default';
+import { ThemeProvider } from '@/themes/default/context/ThemeContext';
+import { ProductProvider } from '@/providers/Product';
+import { OrderProvider } from '@/providers/Order';
+import '@/App.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <ProductProvider>
       <OrderProvider>
         <ThemeProvider>
-          <MainProvider>
-            <ThemeDefault>
-              <ToastContainer
-                position="top-right"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored"
-              />
-              <Outlet />
-            </ThemeDefault>
-          </MainProvider>
+          <ThemeDefault>
+            <ToastContainer
+              position="top-right"
+              autoClose={2000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+            />
+            <Outlet />
+          </ThemeDefault>
         </ThemeProvider>
       </OrderProvider>
     </ProductProvider>
